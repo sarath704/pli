1b442d8965612b5c803ba74fb7d09b423a9b1f79
Sarath Babu Nuthimadugu <sarath.nuthimadugu@imaginea.com>
2019-01-10 17:31:13 +0530

    navigation added

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,17 @@
-import {Component} from "@angular/core";
+import {Component, OnInit, AfterViewInit} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
 
-@Component({
-  selector: "app-home",
- templateUrl: "./home.component.html",
-styleUrls:["./home.component.scss"]
-})
-export class HomeComponent {}
+@Component({selector: "app-home", templateUrl: "./home.component.html", styleUrls: ["./home.component.scss"]})
+export class HomeComponent  {
+  private fragment: string;
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  goToLink(e, link) {
+    e.preventDefault();
+    const x = document.querySelector(link);
+    if (x) {
+        x.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  }
+
+}

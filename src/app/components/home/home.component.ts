4cf3c703cd9571103023adf307fdf04bd9a83b3b
sarath <greekysarath@gmail.com>
2019-01-11 02:43:50 +0530

    new designs

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import {Component, OnInit, AfterViewInit} from "@angular/core";
 import {ActivatedRoute, Router} from "@angular/router";
+import { AuthService } from "../../shared/services/auth.service";
 
 @Component({selector: "app-home", templateUrl: "./home.component.html", styleUrls: ["./home.component.scss"]})
 export class HomeComponent  {
   private fragment: string;
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  constructor(private route: ActivatedRoute, private router: Router, public authService: AuthService,) {}
 
   goToLink(e, link) {
     e.preventDefault();
